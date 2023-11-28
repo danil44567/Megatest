998f0dd2298af640cfacf2c28c4fccee3912e34d
aT <krivonocovf@gmail.com>
2023-11-28 12:37:34 +0500

    add qustion_arr (need fixes)

--- xdddddddzs.js
+++ xdddddddzs.js
@@ -1,21 +1,43 @@
 const iconsFloder = "img/icons/"
 const scoreContainer = document.querySelector(".test_list_container")
 
+async function get_question_count(file){
+    let questionCount =  await fetch(`tests/${file}.json`)
+    .then(response => response.json())
+    .then(test =>{console.log(test)
+        return test.questions.length
+    })
+    return questionCount
+}
 
+let usersTests = JSON.parse(localStorage.testsave)
 
 fetch(`test-data.json`)
     .then(response => response.json())
     .then(json => {
         let allTests = json.tests
-        let usersTests = JSON.parse(localStorage.testsave)
+        
+        let promises=[]
         usersTests.tests.forEach(element => {
             let currentTest = allTests[element.testid]
-            /*let questionCount = fetch(`tests/${currentTest.file}.json`)
-            .then(response => response.json())
-            .then(test =>{console.log(test)
-                return test.questions.length
-            })*/
-            console.log(questionCount)
+            promises.push(get_question_count(currentTest.file))
+            // console.log(questionCount)
+            // const resultsContainer = document.createElement("div")
+            // resultsContainer.classList.add("test_list_element")
+            // let icon = iconsFloder + (currentTest.icon == "" ? "EmptyIcon.png" : currentTest.icon)
+            // resultsContainer.innerHTML =
+            //     `
+            //     <img src="${icon}" alt="test icon">
+            //     <div class="test_name">${currentTest.title}</div>
+            //     <div class="test_score">${element.result}</div>
+            // `
+            // scoreContainer.appendChild(resultsContainer)
+    })
+    let qustion_arr=Promise.all(promises).then(a=>{
+        for (let index = 0; index < a.length; index++) {
+            //usersTests.test[index]
+            //a[i]
+            let currentTest = usersTests.tests[index]
             const resultsContainer = document.createElement("div")
             resultsContainer.classList.add("test_list_element")
             let icon = iconsFloder + (currentTest.icon == "" ? "EmptyIcon.png" : currentTest.icon)
@@ -23,9 +45,10 @@ fetch(`test-data.json`)
                 `
                 <img src="${icon}" alt="test icon">
                 <div class="test_name">${currentTest.title}</div>
-                <div class="test_score">${element.result}</div>
+                <div class="test_score">${usersTests.tests[index].result}/${a[index]}</div>
             `
             scoreContainer.appendChild(resultsContainer)
+        }
     })
     })
     console.log(JSON.parse(localStorage.testsave))
