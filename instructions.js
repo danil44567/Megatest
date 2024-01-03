a819736217dad2ca18f47536135bac1a268b1787
aTule-csc <113103385+aTule-csc@users.noreply.github.com>
2024-01-04 03:39:56 +0500

    toggleAll is not working, else is fine

--- instructions.js
+++ instructions.js
@@ -1,44 +1,46 @@
-
-
 function toggleAll(counter)
 {
     if (counter = 1)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
         document.getElementById("text").textContent ='silly cat1'
-        document.getElementById("page_number").innerHTML =1
     }
     else if (counter = 2)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
         document.getElementById("text").textContent ='silly cat2'
-        document.getElementById("page_number").innerHTML ='<img src="img/instructions/but.png" alt="кнопки">'
     }
     else if (counter = 3)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
         document.getElementById("text").textContent ='silly cat3'
-        document.getElementById("page_number").innerHTML ='<img src="img/instructions/but.png" alt="кнопки">'
     }
     else if (counter = 4)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
         document.getElementById("text").textContent ='silly cat4'
-        document.getElementById("page_number").innerHTML ='<img src="img/instructions/but.png" alt="кнопки">'
     }    
 }
 let count
-count = counter
+
 
 function plusOne(count)
 {  
-    count++
+    count = document.getElementById("page_number").textContent
     console.log(count)
+    if (count <4 ) count++
+    console.log(count)
+    document.getElementById("page_number").innerHTML = count
+    console.log(document.getElementById("page_number").textContent)
+    toggleAll(document.getElementById("page_number").textContent)
 }
 function minusOne(count)
 {  
-    
-    count--
+    count = document.getElementById("page_number").textContent
+    if (count >1 )count--
     console.log(count)
+    document.getElementById("page_number").innerHTML = count
+    toggleAll(document.getElementById("page_number").textContent)
 }
-document.getElementById('button_right').addEventListener('click', toggleAll(2));
+document.getElementById('button_right').addEventListener('click', plusOne);
+document.getElementById('button_left').addEventListener('click', minusOne);
