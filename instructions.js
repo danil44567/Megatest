c1bd1adc21aafd6d3dffea0bbaf02a71c51db28c
aTule-csc <113103385+aTule-csc@users.noreply.github.com>
2024-01-05 16:33:34 +0500

    inst-breakpoints.css added

--- instructions.js
+++ instructions.js
@@ -3,22 +3,22 @@ function toggleAll(counter)
     if (counter == 1)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
-        document.getElementById("text").textContent ='silly cat1'
+        document.getElementById("text").textContent ='Первым делом вы сразу попадёте на главную. Вы можете выбрать тест, нажав на него, и перейти к его прохождению. Хорошей идеей будет попробовать что-нибудь из вкладки “Лучшие тесты”. Не можете найти то, что интересно вам? Попробуйте найти это с помощью фильтров – нажимайте на кнопку с интересующим фильтром и вам будет показывать тесты только из этой категории. Если хотите отменить фильтрацию, то нажмите на кнопку “Все”.'
     }
     else if (counter == 2)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
-        document.getElementById("text").textContent ='После выбора теста вы перейдёте к его прохождению: читайте вопрос нажимайте на кнопку с ответом который считаете правильным и после пары секунд ожидания вы перейдёте следующему вопросу. После нажатия ответ загориться зелёным если он был правильный и красным если неправильный. В тесте нельзя вернуться к предыдущим вопросам и выбрать другой вариант, так что учитывайте это во время прохождения.'
+        document.getElementById("text").textContent ='После выбора теста вы перейдёте к его прохождению: читайте вопрос нажимайте на кнопку с ответом, который считаете правильным и после пары секунд ожидания вы перейдёте следующему вопросу. После нажатия ответ загорится зелёным если он был правильный и красным если неправильный. В тесте нельзя вернуться к предыдущим вопросам и выбрать другой вариант, так что учитывайте это во время прохождения.'
     }
     else if (counter == 3)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
-        document.getElementById("text").textContent ='silly cat3'
+        document.getElementById("text").textContent ='Пройдя тест, результаты можно посмотреть в истории решений, нажав на соответствующую кнопку в шапке сайта.'
     }
     else if (counter == 4)
     {
         document.getElementById("screen").innerHTML ='<img src="img/instructions/Rectangle 29.png" alt="скрин">'
-        document.getElementById("text").textContent ='silly cat4'
+        document.getElementById("text").textContent ='Если вы хотите связаться с нами, то вся контактная информация находится в подвале сайте, а если вы хотите побольше узнать о нас, то может нажать на соответствующую кнопку в подвале/шапке сайта.'
     }    
 }
 let count
@@ -27,9 +27,7 @@ let count
 function plusOne(count)
 {  
     count = document.getElementById("page_number").textContent
-    console.log(count)
     if (count <4 ) count++
-    console.log(count)
     document.getElementById("page_number").innerHTML = count
     console.log(document.getElementById("page_number").textContent)
     toggleAll(document.getElementById("page_number").textContent)
@@ -37,8 +35,7 @@ function plusOne(count)
 function minusOne(count)
 {  
     count = document.getElementById("page_number").textContent
-    if (count >1 )count--
-    console.log(count)
+    if (count >1 ) count--
     document.getElementById("page_number").innerHTML = count
     toggleAll(document.getElementById("page_number").textContent)
 }
